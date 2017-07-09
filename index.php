a689e3518536f9376c88e8fdef2977836b2d3254
jesikaAz2017 <j.anschutz@laposte.net>
2017-07-09 23:02:54 +0200

    Update PHP2

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Challenge-PHP2</title>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body> <!-- Les tests en PHP  -->
+
+    <h1>Challenge n°2 sur PHP</h1>
+    
+   
+    <div>
+    
+        <h2>Exercice 1 - Afficher les n premiers chiffres</h2>
+
+        <p> Vous devez réaliser une fonction d'affichage. <br> La fonction prend en paramètres 2 nombres qui correspondent au début et à la fin d’une plage de nombres. </p>
+
+    </div>
+    
+     <div>
+        
+        <h2>Exercice 2- Afficher le plus grand gap</h2>
+    
+        <p>Créer un algorithme qui calcule le plus grand écart entre deux entiers consecutifs dans un tableau passé en paramètre d'entrée.</p> 
+        
+    </div> 
+
+    <div>
+        
+        <h2>Exercice 3 - Jeu de la fourchette</h2>
+
+        <p> Simuler le jeu de la fourchette. Ce jeu consiste à essayer de découvrir un nombre quelconque compris entre 1 et 100 inclus, tiré au sort par l’ordinateur (la primitive rand(min, max) retourne un entier entre min et max). L’utilisateur a droit à huit essais maximum. À chaque essai, vous devrez afficher un message indicatif « nombre donné trop petit » ou « nombre donné trop grand ». En conclusion, soit « bravo, vous avez trouvé en [nombre] essai(s) » soit « désolé, le nombre était [valeur] ».</p>
+        
+    </div>   
+    
+</body>
+
+</html>
